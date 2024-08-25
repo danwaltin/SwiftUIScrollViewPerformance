f59adecd315dcc7a7f8360c6d832f4b293d043a4
Dan Waltin <dan@waltin.se>
2024-08-25 10:49:04 +0200

    adding project

---
+++ SwiftUIScrollViewPerformance/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIScrollViewPerformance
+//
+//  Created by Dan Waltin on 2024-08-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIScrollViewPerformance/SwiftUIScrollViewPerformanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIScrollViewPerformanceApp.swift
+//  SwiftUIScrollViewPerformance
+//
+//  Created by Dan Waltin on 2024-08-25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIScrollViewPerformanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
