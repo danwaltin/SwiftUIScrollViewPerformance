bb4589864c644fdbcd155f36a4eea63508863f30
Dan Waltin <dan@waltin.se>
2024-09-01 17:46:50 +0200

    Project format for xcode 15

--- SwiftUIScrollViewPerformance/Views/Timeline/TimelineSectionView.swift
+++ SwiftUIScrollViewPerformance/Views/Timeline/TimelineSectionView.swift
@@ -10,13 +10,32 @@ import SwiftUI
 struct TimelineSectionView: View {
 	let section: TimelineSection
 	
+	@State private var isVisible = true
+
 	var body: some View {
-		content()
-			.frame(width: section.displayWidth)
+		if #available(macOS 15.0, *) {
+			content()
+				.frame(width: section.displayWidth)
+//				.onScrollVisibilityChange(threshold: 0.01) {
+//					isVisible = $0
+//				}
+		} else {
+			content()
+				.frame(width: section.displayWidth)
+		}
 	}
 	
 	@ViewBuilder
 	private func content() -> some View {
+		if isVisible {
+			visibleContent()
+		} else {
+			invisibleContent()
+		}
+	}
+	
+	@ViewBuilder
+	private func visibleContent() -> some View {
 		SectionTicks(ticks: section.ticks)
 			.stroke(.green, lineWidth: 1)
 			.overlay(alignment: .topLeading) {
@@ -27,5 +46,10 @@ struct TimelineSectionView: View {
 				SectionTime(time: section.displayEndTime)
 					.padding(.trailing, -1)
 			}
+	}
+	
+	@ViewBuilder
+	private func invisibleContent() -> some View {
+		Color.clear.allowsHitTesting(false)
 	}
 }
